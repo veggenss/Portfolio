00141e8bf5a16cba1495a2f68a0dada02faf2cb9
unicfoxx <94608475+unicfoxx@users.noreply.github.com>
2024-12-09 13:04:55 +0100

    Delete JS/skole.js

--- JS/skole.js
+++
@@ -1,7 +0,0 @@
-console.log("Epix Math Calculator sigma + ligma");
-
-let ligma = prompt("input ligma number here:");
-let sigma = prompt("input sigma number here:");
-
-console.log(ligma + sigma);
-
