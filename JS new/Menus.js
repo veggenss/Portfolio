edafbb37b85ea83b7166e69589da0cb70f36fbf9
John Doe <unicfoxx@gmail.com>
2025-03-21 13:02:58 +0100

    dsdf

--- JS new/Menus.js
+++ JS new/Menus.js
@@ -121,4 +121,80 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+// Page Navigation
+// Drops a navigation menu to make it easier to move around the page
+document.addEventListener('DOMContentLoaded', () => {
+    const topbar = document.getElementById('topbar');
+    const pagenav = document.getElementById('topbar-pagenav');
+    let timeout2;
+
+    topbar.addEventListener('mouseenter', () => {
+        clearTimeout(timeout2); 
+        pagenav.classList.remove('nodisplay2'); 
+    });
+
+    topbar.addEventListener('mouseleave', () => {
+        timeout2 = setTimeout(() => {
+            pagenav.classList.add('nodisplay2');
+        }, 200);
+    });
+
+    pagenav.addEventListener('mouseenter', () => {
+        clearTimeout(timeout2); 
+        pagenav.classList.remove('nodisplay2'); 
+    });
+
+    pagenav.addEventListener('mouseleave', () => {
+        timeout2 = setTimeout(() => {
+            pagenav.classList.add('nodisplay2');
+        }, 200);
+    });
+});
+
+window.onload = function cookieChecker() {
+    var element = document.body;
+
+    // Check if light mode cookie exists, if so, apply light mode
+    if (document.cookie.includes('light=true')) {
+        element.classList.toggle("light-mode");
+    }
+
+    // Check if grandma mode cookie exists, if so, apply grandma mode
+    if (document.cookie.includes('grandma=true')) {
+        element.classList.toggle("grandma");
+    }
+}
+
+// Light Mode Toggle
+// Adds or removes a class from the classlist that changes the site from pink to light mode
+function lightmodeToggle() {
+
+    // Toggle light mode cookie
+    if (document.cookie.includes('light=true')) {
+        // Remove cookie by setting expiration to a past date
+        document.cookie = "light=true; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    } else {
+        // Set cookie for light mode
+        document.cookie = 'light=true; path=/;';
+        var element = document.body;
+        element.classList.toggle("light-mode");
+    }
+}
+
+// Grandma Mode Toggle
+// Adds or removes a class from the classlist that changes the site from pink to light mode
+function grandmamodeToggle() {
+
+    // Toggle light mode cookie
+    if (document.cookie.includes('grandma=true')) {
+        // Remove cookie by setting expiration to a past date
+        document.cookie = "grandma=true; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    } else {
+        // Set cookie for light mode
+        document.cookie = 'grandma=true; path=/;';
+        var element = document.body;
+        element.classList.toggle("grandma");
+    }
+}
+
 console.log('Completed scriptsheet');
